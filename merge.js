14e1bfbbe32e20fca48a3dc77ae37be87223a96a
Nikhil <nk4338996@gmail.com>
2024-03-15 11:23:15 +0530

    Added promise syntax in merge.js

--- merge.js
+++ merge.js
@@ -8,39 +8,42 @@ const getPageCount = async (pdfPath) => {
 };
 
 const mergePdfs = async (p1, p2, pdf1, pdf2) => {
-  // Use dynamic import within the async function
-  const PDFMergerModule = await import("pdf-merger-js");
-  const PDFMerger = PDFMergerModule.default;
-  const merger = new PDFMerger();
+  return new Promise(async (resolve,reject)=>{
 
-  const pdf1TPage = await getPageCount(p1);
-  const pdf2TPage = await getPageCount(p2);
-
-  if (!isNaN(pdf1) && !isNaN(pdf2)) {
-    await merger.add(p1, `${pdf1} to ${pdf1TPage}`);
-    await merger.add(p2, `${pdf2} to ${pdf2TPage}`);
-    let d = new Date().getTime();
-    await merger.save(`./public/${d}.pdf`);
-    return d;
-  } else if (isNaN(pdf1) && !isNaN(pdf2)) {
+    // Use dynamic import within the async function
+    const PDFMergerModule = await import("pdf-merger-js");
+    const PDFMerger = PDFMergerModule.default;
+    const merger = new PDFMerger();
+    
+    const pdf1TPage = await getPageCount(p1);
+    const pdf2TPage = await getPageCount(p2);
+    
+    if (!isNaN(pdf1) && !isNaN(pdf2)) {
+      await merger.add(p1, `${pdf1} to ${pdf1TPage}`);
+      await merger.add(p2, `${pdf2} to ${pdf2TPage}`);
+      let d = new Date().getTime();
+      await merger.save(`./public/${d}.pdf`);
+      resolve(d);
+    } else if (isNaN(pdf1) && !isNaN(pdf2)) {
+      await merger.add(p1);
+      await merger.add(p2, `${pdf2} to ${pdf2TPage}`);
+      let d = new Date().getTime();
+      await merger.save(`./public/${d}.pdf`);
+      resolve(d);
+    } else if (!isNaN(pdf1) && isNaN(pdf2)) {
+      await merger.add(p1, `${pdf1} to ${pdf1TPage}`);
+      await merger.add(p2);
+      let d = new Date().getTime();
+      await merger.save(`./public/${d}.pdf`);
+      resolve(d);
+    }
+    
     await merger.add(p1);
-    await merger.add(p2, `${pdf2} to ${pdf2TPage}`);
-    let d = new Date().getTime();
-    await merger.save(`./public/${d}.pdf`);
-    return d;
-  } else if (!isNaN(pdf1) && isNaN(pdf2)) {
-    await merger.add(p1, `${pdf1} to ${pdf1TPage}`);
     await merger.add(p2);
     let d = new Date().getTime();
     await merger.save(`./public/${d}.pdf`);
-    return d;
-  }
-
-  await merger.add(p1);
-  await merger.add(p2);
-  let d = new Date().getTime();
-  await merger.save(`./public/${d}.pdf`);
-  return d;
+    resolve(d);
+  })
 };
 
 // Export the async function
